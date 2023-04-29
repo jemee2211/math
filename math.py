e0b4c6e612dc4c3c81b4561250a7deaaed45ddad
Jemi patel <120568492+jemee2211@users.noreply.github.com>
2023-04-29 22:30:25 +0200

    Add files via upload

---
+++ math.py
@@ -0,0 +1,12 @@
+import math
+x = 2
+y = -3
+z = 4
+
+print(round(x))
+print(math.ceil(x))
+print(math.floor(y))
+print(abs(y))
+print(pow(x,2))
+print(min(x,y,z))
+print(max(x,y,z))
---
+++ scanf1.py
@@ -0,0 +1,8 @@
+name = input("What is Your Name")
+print("Name = ",name )
+
+age = int(input("What is Your Age :"))
+print("My Age Is = "+str(age))
+
+height = input("What Is Your Height:")
+print("My Height Is = ",height)
